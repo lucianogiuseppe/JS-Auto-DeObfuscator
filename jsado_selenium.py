646d7481a7cda00f160ea3ed869e4776492a9cc7
peppe <peppe@peppe-System-Product-Name.(none)>
2012-10-15 23:56:40 +0200

    Browser supported: firefox, chrome

---
+++ jsado_selenium.py
@@ -0,0 +1,159 @@
+#!/usr/bin/python
+
+# JS Auto DeObfuscator with Selenium support v. 0.1 by Luciano Giuseppe
+# Useful on deobfuscation by a function as eval
+#
+#Dependencies: Selenium for python: http://pypi.python.org/pypi/selenium, Selenium server:http://seleniumhq.org/download/
+#More infos: http://pypi.python.org/pypi/selenium
+
+import subprocess
+import sys
+import os
+import string
+import random
+#for selenium
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+#JS Auto DeObfuscator
+class jsado:
+	#you can personalize these:
+	browserName = "firefox" #browser supported: chrome or firefox
+	outputFileName = "t.html"
+	
+	
+	def __init__(self):
+		self.fileTxt = None;
+		self.outputFileName = os.getcwd()+"/"+ self.outputFileName
+		self.outputUrlName = "file:///"+self.outputFileName
+
+	#return a random string 
+	def __r(self):
+		size = random.randint(4, 12)
+		return ''.join(random.choice(string.ascii_letters) for x in xrange(size))
+
+	#get the js code to inject into page
+	def __getHackString(self,f, l):
+		hackStr = string.Template("""<script src="http://jsbeautifier.org/beautify.js" type="application/javascript"></script>
+		<script type="application/javascript">
+		(function () {
+		var $memFct = $function;
+		$cont = 0;
+		$str = "";
+		$function = function(ttt) {
+			$cont++;		
+			$str += $cont+ ")" +ttt +"\\n\\n";
+			if($cont == 1) {
+				var $div = document.createElement("div");
+				$div.setAttribute("style", "width:95%; height:95%; overflow:no;z-index:100;position:absolute;bottom:0;left:0;border:1px solid black; background:white;");
+				var $button = document.createElement("button");
+				$button.innerHTML = "Minimize";
+				$button.addEventListener("click", function() {
+					var $p = this.parentNode;				
+					if($p.style.height=="30px") {
+						$p.childNodes[1].style.display="block";
+						$p.style.height="95%";
+						this.innerHTML="Minimize";
+					} else {
+						$p.childNodes[1].style.display="none";
+						$p.style.height="30px";
+						this.innerHTML="Maximize";
+					}
+				});
+				$div.appendChild($button);
+				$code = document.createElement("textarea");
+				$code.setAttribute("readonly", "readonly");
+				$code.setAttribute("style","display:block; height:95%; width:98%; overflow:auto");
+				$div.appendChild($code);
+				if(document.body === undefined || document.body === null)	{		
+					addEventListener("load", function() {
+						$code.innerHTML = js_beautify($str);
+						document.body.appendChild($div);
+					});
+				} else {
+					$code.innerHTML = js_beautify($str);
+					document.body.appendChild($div);
+				}
+	
+			} else
+				if(document.body !== undefined || document.body !== null)	
+					$code.innerHTML += "\\n\\n"+ js_beautify($cont+ ")" +ttt +"\\n\\n");
+
+			if($cont <= $limit) { return $memFct(ttt); }
+
+		};
+		})()</script>\n""");
+		return hackStr.substitute({'function': f, 'limit' : l, 'memFct' : self.__r(), 'cont': self.__r(), 'str' : self.__r(), 'div': self.__r(), 'code': self.__r(), 'oldOnError' : self.__r(), 'button' : self.__r(), 'p' : self.__r()})
+
+	#apply the hack to webpage for de-obfuscate the js code
+	def applyHack(self, fileInput, functionName, limitExecution):
+		try:
+			if self.fileTxt is None:
+				f = open(fileInput, 'r');
+				self.fileTxt = f.read()
+				f.close()
+			outFile = open(self.outputFileName, 'w')
+			outFile.write(self.__getHackString(functionName, limitExecution)+self.fileTxt)
+			outFile.close()
+		except IOError as e:
+			print("I/O error({0}): {1}".format(e.errno, e.strerror))
+			return 0
+		else:
+			return 1
+
+# Main
+if __name__ == "__main__":
+	print("JS Auto DeObfuscator with Seleniumv. 0.1\n")	
+
+	#checks the args
+	argLen = len(sys.argv)	
+	execLimit = 0
+	if (argLen < 3):
+		print( os.path.basename(__file__)+" file.html function_to_hack [n_execution]")
+		sys.exit()
+	elif(argLen == 4):
+		try:	
+			execLimit = int(sys.argv[3])
+		except ValueError:
+	    		print("Bad n_execution value: assume it as 0")
+	
+	try:	
+		hack = jsado()
+		#apply the hack to webpage
+		if(hack.applyHack(sys.argv[1], sys.argv[2], execLimit) == 0):
+			print("An error occurred: byee!")
+			sys.exit()
+		
+		#try to run the browser with the decrypter webpage
+		print("Work completed: %s running..."%hack.browserName)
+		if hack.browserName == "chrome":
+			browser = webdriver.chrome.webdriver.WebDriver(executable_path='chromium-browser', port=9515);
+
+		else:
+			browser = webdriver.Firefox() # Get local session of firefox
+	
+		browser.get(hack.outputUrlName) # Load page
+		print("If there're some ReferenceError errors in js console or the js deobuscated shows strange strings or seems to be obfuscated use increment!")
+	
+		#User want to increment the times that eval is normally executed
+		answer = raw_input("\nDo you want to increment the n_execution? y/n : ")
+		while (answer == "y"):
+			execLimit += 1
+			if (hack.applyHack(sys.argv[1], sys.argv[2], execLimit) == 0):
+				print("An error occurred: byee!")
+				sys.exit()
+			print("Limit:%d - Page refreshing...\n"%execLimit)
+			browser.refresh()
+			answer = raw_input("Do you want to increment the n_execution? y/n : ")
+	except WebDriverException as e:
+		print("Selenium error: %s\n"%e.msg.strip())
+		sys.exit(1)
+	except Exception as e:
+		print("Error: %s\n"%e)
+		sys.exit(1)
+	
+	#exit
+	print("Bye bye!!")
+	
